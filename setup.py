c04d6063ace9cfd64cb4672cb8926df47b933378
Ari Bal <abal@chisel.ai>
2019-02-05 11:37:27 -0500

    fix setup.py for pip>10

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 import os
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+
+# from pip.req import parse_requirements
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 install_reqs = parse_requirements('requirements.txt', session=False)
 test_reqs = parse_requirements('requirements_test.txt', session=False)
 
 version = '0.6.1'
 
-README="""Python class to integrate Boto3's Cognito client so it is easy to login users. With SRP support."""
+README = """Python class to integrate Boto3's Cognito client so it is easy to login users. With SRP support."""
 
 setup(
     name='warrant',
